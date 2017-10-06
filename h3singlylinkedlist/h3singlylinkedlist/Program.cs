b86472bd87179348d138afefd4323c867540276f
Şevket Umut Çakır <sucakir@pau.edu.tr>
2017-10-06 12:55:48 +0300

    06.10.2017 birinci öğretim ders

--- h3singlylinkedlist/h3singlylinkedlist/Program.cs
+++ h3singlylinkedlist/h3singlylinkedlist/Program.cs
@@ -16,7 +16,21 @@ namespace h3singlylinkedlist
             Node d3 = new Node(8, null);
             d2.next = d3;
             SinglyLinkedList ll = new SinglyLinkedList(d1);
-            ll.print();
+            //ll.print();
+            //Console.WriteLine(ll.toplam());
+            SinglyLinkedList bl2 = new SinglyLinkedList();
+            bl2.addHead(3);
+            bl2.addHead(5);
+            bl2.addHead(7);
+            bl2.print();
+            bl2.addTail(9);
+            bl2.print();
+            bl2.insertAfter (bl2.getHead (), 15);
+            bl2.print();
+            bl2.insertAfter(0, 76);
+            bl2.insertAfter(5, 41);
+            bl2.addHead(12);
+            bl2.print();
         }
     }
 }
--- h3singlylinkedlist/h3singlylinkedlist/SinglyLinkedList.cs
+++ h3singlylinkedlist/h3singlylinkedlist/SinglyLinkedList.cs
@@ -25,8 +25,62 @@ namespace h3singlylinkedlist
         }
         public int toplam()
         {
-            //Toplama kodu yazılacak.
-            return -1;
+            Node current = head;
+            int t = 0;
+            while (current != null)
+            {
+                t += current.value;
+                current = current.next;
+            }
+            return t;
+        }
+        public Node getHead()
+        {
+            return head;
+        }
+        public void addHead(int value)
+        {
+            Node newNode = new Node(value, head);//Yeni oluşturulan düğüm head düğümünü göstersin
+            head = newNode;//Yeni başlangıç değerini yeni oluşturulan düğüm olarak belirle
+        }
+        public void addTail(int value)
+        {
+            Node newNode = new Node(value, null);//Yeni düğüm
+            if (head == null)//Bağlı liste boş ise
+            {
+                head = newNode;//Başa ekle
+                return;//Geri dön
+            }
+            Node current = head;//Başlangıç düğümü
+            while (current.next != null)//Son elemana kadar döngü devam edecek(null olmadan döngü sonlanacak)
+            {
+                current = current.next;//Mevcut değeri belirle
+            }
+            //current şu an son elemanı gösteriyor
+            current.next = newNode;//son eleman olarak bağlantıyı ekle
+        }
+        /*
+         * Parametre olarak verilen node düğümünden sonra araya bir eleman ekler
+         */ 
+        public void insertAfter(Node node, int value)
+        {
+            Node newNode = new Node(value, node.next);//Yeni düğüm oluştur. Yeni düğümün sonraki elemanı parametre olarak gönderilen düğümün sonraki değeri olarak ata.
+            node.next = newNode;//düğümün(node) sonraki değerini yeni düğüm olarak belirle
+        }
+        /*
+         * Parametre olarak verilen indisteki düğümünden sonra araya bir eleman ekler
+         */
+        public void insertAfter(int index, int value)
+        {
+            int i = 0;//Başlangıç indisi
+            Node current = head;//head düğümünden başla
+            while (i < index)//parametre olarak verilen indis değerine gelene kadar döngü devam eder
+            {
+                i++;
+                current = current.next;//sonraki elemana geç
+            }
+            //Döngüden çıktıktan sonra current istenilen indisteki düğümü saklar
+            insertAfter(current, value);//Yukardaki insertAfter metodunu çağırarak elemanı araya ekle
         }
     }
 }
