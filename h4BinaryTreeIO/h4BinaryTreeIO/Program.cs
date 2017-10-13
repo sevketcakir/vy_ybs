d53e98559332d3b4d67e4313cee4ba7c0118a12b
Şevket Umut Çakır <sucakir@pau.edu.tr>
2017-10-13 20:00:13 +0300

    13.10.2017 ikinci öğretim ders

---
+++ h4BinaryTreeIO/h4BinaryTreeIO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h4BinaryTreeIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *      41
+             *     /  \
+             *    18  64
+             *    ağacını oluşturalım.
+             */
+            TreeNode kok = new TreeNode(41);//Kök düğümünü oluştur
+            TreeNode sol=new TreeNode(18);//Sol çocuğu oluştur ve bağla
+            kok.left=sol;
+            kok.right = new TreeNode(64);//Sağ çocuğu oluştur ve bağla
+            //kok.right.right.left.right.left
+        }
+    }
+}
---
+++ h4BinaryTreeIO/h4BinaryTreeIO/TreeNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h4BinaryTreeIO
+{
+    class TreeNode
+    {
+        public int value;//Değer
+        public TreeNode left;//Sol alt çocuk
+        public TreeNode right;//Sağ alt çocuk
+        public TreeNode() { }
+        public TreeNode(int value)
+        {
+            this.value = value;
+        }
+        public TreeNode(int value, TreeNode left, TreeNode right)
+        {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
