1f52c8b24b3a8d97e9dafddc726f77eb2a0a8a31
Şevket Umut Çakır <sucakir@pau.edu.tr>
2017-10-13 12:30:54 +0300

    13.10.2017 birinci öğretim ağaç ve ağaç düğümü

---
+++ h4BinaryTree/h4BinaryTree/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h4BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *       20
+             *      /  \
+             *     15  41
+             *     ağacını oluşturan kodu yazalım
+             */
+            TreeNode kok = new TreeNode(20);
+            kok.left = new TreeNode(15);
+            kok.right = new TreeNode(41);
+        }
+    }
+}
---
+++ h4BinaryTree/h4BinaryTree/TreeNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace h4BinaryTree
+{
+    class TreeNode
+    {
+        public int value;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode() { }
+        public TreeNode(int value)
+        {
+            this.value = value;
+        }
+        public TreeNode(int value, TreeNode left, TreeNode right)
+        {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
