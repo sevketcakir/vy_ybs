6068a336194b4fa4df59011f7f70f4e7c4cb5498
Şevket Umut Çakır <sucakir@pau.edu.tr>
2017-10-20 11:39:48 +0300

    System.Collections.Generic içindeki Stack kullanımı

---
+++ StackKullanimi/StackKullanimi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackKullanimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> s = new Stack<int>();//Stack oluştur
+            s.Push(15);//Stack'e ekle
+            s.Push(7);//Stack'e ekle
+            int sonuc = s.Pop() / s.Pop();//Stack'ten 2 değer çek ve işlem yap
+            Console.WriteLine(sonuc);//Sonucu yazdır
+            "1 2 + 5 4 - /".Split(' ');//Metni boşluğa göre parçalar
+
+        }
+    }
+}
