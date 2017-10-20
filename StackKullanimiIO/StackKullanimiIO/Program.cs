aa29c0fee7086487cca635a9afce7b2976d0550a
Şevket Umut Çakır <sucakir@pau.edu.tr>
2017-10-20 19:50:46 +0300

    20.10.2017 ikinci öğretim ders

---
+++ StackKullanimiIO/StackKullanimiIO/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackKullanimiIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> s = new Stack<int>();//Stack oluştur
+            s.Push(17);//Stack'e değer it
+            s.Push(6);//Stack'e değer it
+            int sonuc = s.Pop() / s.Pop();//2 değer çek ve işlem yap
+            Console.WriteLine(sonuc);//sonucu ekrana yazdır
+            var a = "3 2 + 5 16 - /".Split(' ');//Boşluk karakterine göre metni parçala
+        }
+    }
+}
